7ee5de27d59cc77d6fe5b6dbf372885040175d5b
guneyguven <guneyguven@gmail.com>
2022-05-13 12:43:06 +0300

    footerdone

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '<div class="container">
+<h2 class="section-title mb-5">work together</h2>
+<a href="#" class="site-btn">Get in touch</a>
+<div class="social-links">
+  <a href="#"><img class="scimg" src="img/Octocat.png"></a>
+  <a href="#"><img class="scimg" src="img/instagram.png"></span></a>
+  <a href="#"><img class="scimg" src="img/facebook.png"></span></a>
+  
+</div>
+<div class="copyright">
+  Copyright &copy;<script>
+    document.write(new Date().getFullYear());
+  </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by
+  <a href="http://www.guneyguven.com" target="_blank">guneyguven</a>
+
+</div>
+</div>'
+
+?>
