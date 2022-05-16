1889fe38f4982c43a5be1ab04d8382b3e44756e1
guneyguven <guneyguven@gmail.com>
2022-05-16 10:59:25 +0300

    togglerbetter

--- js/colorshift.js
+++ js/colorshift.js
@@ -1,7 +1,18 @@
-<script type="text/javascript">
-const chk = document.getElementById('chk');
+<!--colorshifter-->
+<div>
+  <input type="checkbox" class="checkbox" id="chk" />
+  <label class="label" for="chk">
+    <i class="fa-solid fa-sun"></i>
+    <i class="fa-solid fa-moon"></i>
+    <div class="ball"></div>
+  </label>
+</div>
 
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
-});
-</script>
+<script type="text/javascript">
+  const chk = document.getElementById('chk');
+  
+  chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+  });
+  </script>
+<!--colorshifter-->
