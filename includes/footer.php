de048480408f8d987eff39cf3456779e2a116089
guneyguven <guneyguven@gmail.com>
2022-12-14 11:49:02 +0300

    contactphp.phpLoad

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,19 @@
 <?php
 
 echo '<div class="container">
-<h2 class="section-title mb-5">work together</h2>
-<a href="#" class="site-btn">Get in touch</a>
+<h2 class="section-title mb-5">Contact me</h2>
+<a href="contact.html" class="site-btn">Get in touch</a>
 <div class="social-links">
-  <a href="#"><img class="scimg" src="img/Octocat.png"></a>
-  <a href="#"><img class="scimg" src="img/instagram.png"></span></a>
-  <a href="#"><img class="scimg" src="img/facebook.png"></span></a>
+  <a href="https://github.com/guneyguven"><img class="scimg" src="img/social/octocat.png"></a>
+  <a href="https://www.instagram.com/thorun_kankasi"><img class="scimg" src="img/social/instagram.png"></span></a>
+  <a href="https://wwww.facebook.com/guneyguven"><img class="scimg" src="img/social/facebook.png"></span></a>
   
 </div>
 <div class="copyright">
+  
   Copyright &copy;<script>
     document.write(new Date().getFullYear());
-  </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by
+  </script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by 
   <a href="http://www.guneyguven.com" target="_blank">guneyguven</a>
 
 </div>
